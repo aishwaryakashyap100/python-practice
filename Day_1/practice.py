25cc4f5a20506f51969faccd5263844866f8bf68
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-10 11:44:31 +0530

    Day_1

---
+++ Day_1/practice.py
@@ -0,0 +1,8 @@
+#example 1
+name = "John"
+age = 25
+print ("Hello", name, "you are",age,"years old")
+#example 2
+age = input("Enter your age: ")  #this is a string by default
+age = int(age)                   #convert string to integer
+print ("you will be", age+1, "next year")
