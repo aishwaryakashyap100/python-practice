6930cd2500de9c31fcfe8d7b87c0c15aa0810135
Aishwarya Kashyap <aishwaryakashyap100@gmail.com>
2025-07-10 12:10:25 +0530

    Day_2

---
+++ Day_2/practice.py
@@ -0,0 +1,17 @@
+#example 1
+x = 10          # Storing value 10 in x
+if x == 10:     # Checking if x is equal to 10
+    print("x is 10")
+#example 2
+age = 20
+has_id = True
+
+if age >= 18 and has_id:
+    print("Entry allowed")
+#example 3
+num = int(input("Enter number: "))
+
+if num % 2 == 0 and num % 5 == 0:
+    print("✅ Number is even and divisible by 5")
+else:
+    print("❌ Condition not met")
